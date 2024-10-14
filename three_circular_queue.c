671fbb0d3ca634e7eff83d1029d902c3a0859dce
SubramanyaJ <subramanyaj7@gmail.com>
2024-10-14 08:52:39 +0530

    Trial, curl
    Signed-off-by: SubramanyaJ <subramanyaj7@gmail.com>

---
+++ three_circular_queue.c
@@ -0,0 +1,9 @@
+/**
+ * 3b) WAP to simulate the working of a circular queue of
+ * integers using an array. Provide the following operations:
+ * Insert, Delete & Display
+ * The program should print appropriate messagesfor queue
+ * empty and queue overflow conditions
+ */
+
+
---
+++ three_queue.c
@@ -0,0 +1,16 @@
+/* *
+ * 3a) WAP to simulate the working of a queue of integers
+ * using an array. Provide the following operations: Insert,
+ * Delete, Display
+ * The program should print appropriate messagesfor queue
+ * empty and queue overflow conditions
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define MAX 10
+
+int myQueue[MAX];
