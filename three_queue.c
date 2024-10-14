129b28de719e72594d3575400d95012e439ef3fd
Subramanya J <149412686+SubramanyaJ@users.noreply.github.com>
2024-10-14 10:51:44 +0530

    Updated three_queue.c

--- three_queue.c
+++ three_queue.c
@@ -13,4 +13,54 @@
 
 #define MAX 10
 
-int myQueue[MAX];
+int queue[MAX];
+int front = -1;
+int back = -1;
+
+void addQueue(int element){
+	
+	if(front == MAX - 1){
+		fprintf(stderr, "Queue is full!\n");
+		return;
+	}
+		
+	queue[++front] = element;
+	return;
+}
+
+int deQueue(){
+	
+	if(back == front){
+		fprintf(stderr, "Queue is empty!\n");
+		return -1;
+	}
+
+	return (queue[++back]);
+
+}
+
+
+int main(){
+	
+	int case = 0;
+	int value = 0;
+	while(true){
+		
+		switch (case){
+		case 1: 
+			printf("Enter a number to queue.\n");
+			scanf("%d", &value);
+			addQueue(value);
+			break;
+		case 2:
+			value = deQueue(); 
+			printf("The deQueue'd value is %d.\n", value);
+		case 3:
+			return 0;
+		case 4:
+			printf("Unknown option! Try again!\n");
+
+		}
+	}
+	return 0;
+}
