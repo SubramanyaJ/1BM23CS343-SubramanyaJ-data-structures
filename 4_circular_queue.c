3165a9fd27beba265f84241aa3841e86a0f95175
Subramanya J <149412686+SubramanyaJ@users.noreply.github.com>
2024-10-21 12:35:54 +0530

    Updated 4_circular_queue.c

--- 4_circular_queue.c
+++ 4_circular_queue.c
@@ -12,21 +12,22 @@
 #include <stdbool.h>
 
 #define MAX 10
+#define DB (printf("control reached here\n"))
 
-#define circArr[MAX]
+int circArr[MAX];
 int front = -1;
 int back = -1;
-int errno = 0;
+int myerrno = 0;
 
 bool isFull(){
-	if( (rear + 1) % MAX == front ){
+	if( (back + 1) % MAX == front ){
 		return true;
 	}
 	return false;
 }
 
 bool isEmpty(){
-	if( front == rear == -1){
+	if( front == -1){
 		return true;
 	}
 	return false;	
@@ -34,7 +35,7 @@ bool isEmpty(){
 
 void enqueue(int data){
 
-	if( isfull() ){
+	if( isFull() ){
 		fprintf(stderr, "The queue is full! Overflow\n");
 		return;
 	}	
@@ -43,8 +44,8 @@ void enqueue(int data){
 		front = 0;
 	}
 
-	rear = (rear + 1) % MAX;
-	circArrr[rear] = data;
+	back= (back+ 1) % MAX;
+	circArr[back] = data;
 
 	printf("%d inserted.\n", data);
 	return;
@@ -54,18 +55,77 @@ int dequeue(){
 
 	if( isEmpty() ){
 		fprintf(stderr, "The queue is empty! Underflow!\n");
-		errno = 1;
+		myerrno = 1;
 		return -1;
 	}
 	
 	int retval = circArr[front];
 
-	if( front == rear ){
-		front = rear = -1;
+	if( front == back){
+		front = back = -1;
 	}
 	else{
 		front = (front + 1) % MAX;
 	}
 
-	return data;
+	return retval;
 }
+
+
+void display(){
+	
+	if( isEmpty() ){
+		fprintf(stderr, "The queue is empty!\n");
+		return;
+	}
+
+	int i;
+	
+	for(int i = front; i != back; i = (i+1) % MAX){
+		printf("%d ", circArr[i]);
+	}	printf("%d \n", circArr[back]);
+
+}
+
+int main(){
+
+	int casev = 0;
+	int value = 0;
+
+	while(true){
+	
+		printf("\nEnter an option :\n1. Enqueue\n2. Dequeue\n3. Display\n4. Exit\n");
+
+		scanf("%d", &casev);
+	
+		switch(casev){
+			
+			case 1:
+				printf("Enter value : ");
+				scanf("%d", &value);
+				enqueue(value);
+				break;
+			case 2:
+				value = dequeue();
+				if(myerrno){
+					myerrno  = 0;
+					break;
+				}
+				printf("Returned value is %d\n", value);
+				break;
+			case 3:
+				display();
+				break;
+			case 4:
+				return 0;
+			default:
+				printf("Invalid option!\n");
+				break;
+	
+			
+		}	// End of switch case
+		
+
+	}		// End of while-true
+	return 0;
+}	// End of main
